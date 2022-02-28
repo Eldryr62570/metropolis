2ca7121034c4114a3f1c3aa574990d6bb3ae4371
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-02-28 18:53:04 +0100

    Front : textes + image 2 premiere section

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/styles/style.css">
+    <title>Metropolis</title>
+</head>
+<body>
+    <div class="header">
+        <a href="#">
+            <div class="button">
+               Sign In
+            </div>
+        </a>
+        <a href="#">
+            <div class="button">
+               Sign up
+            </div>
+        </a>
+       
+    </div>
+    <section class="logo">
+        <div class="filter">
+            <img src="asset/img/metropolis_black.jpg" alt="">
+        </div>
+    </section>
+    <section class="titleText1">
+        <div>Le ciné prés de chez vous !</div>
+    </section>
+    <section class="text1"> 
+        <div class="imgText1Container">
+            <img src="asset/img/metropolis_front.jpg" alt="">
+        </div>
+        <div class="separatorContainer">
+            <div class="separator"></div>
+        </div>
+        <div class="imgText1Container">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident .</p>
+        </div>
+    </section>
+</body>
+</html>
