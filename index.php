8b72c9a9a19b67a39047afe4666d8ab56ede406b
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-03-01 09:13:40 +0100

    Parallax

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,9 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="asset/styles/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="asset/js/parallax.js-1.5.0/parallax.js"></script>
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="asset/js/script.js" defer></script>
+    
     <title>Metropolis</title>
 </head>
 <body>
+<script>
+  AOS.init();
+</script>
     <div class="header">
         <a href="#">
             <div class="button">
@@ -23,7 +34,7 @@
     </div>
     <section class="logo">
         <div class="filter">
-            <img src="asset/img/metropolis_black.jpg" alt="">
+            <img src="asset/img/metropolis_black.jpg" alt="" >
         </div>
     </section>
     <section class="titleText1">
@@ -31,14 +42,19 @@
     </section>
     <section class="text1"> 
         <div class="imgText1Container">
-            <img src="asset/img/metropolis_front.jpg" alt="">
+            <img src="asset/img/metropolis_front.jpg" alt="" data-aos="fade-right" data-aos-duration="1000">
         </div>
         <div class="separatorContainer">
             <div class="separator"></div>
         </div>
-        <div class="imgText1Container">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident .</p>
+        <div class="imgText1Container" data-aos="fade-left" data-aos-duration="1000">
+            <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident .</p>
         </div>
+    </section>
+    <div class="parallax-window" data-parallax="scroll" data-image-src="asset/img/background.jpg"></div>
+    <section class="text2">
+        <div class="part1"></div>
+        <div class="part2"></div>
     </section>
 </body>
 </html>
