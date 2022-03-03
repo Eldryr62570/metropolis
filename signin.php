39c6caf5e799c41bee726aadc018db0ff451483c
Jordan MOULIN <98018776+Eldryr62570@users.noreply.github.com>
2022-03-03 19:46:56 +0100

    sign in

--- signin.php
+++ signin.php
@@ -4,24 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="asset/styles/style.css">
+    <link rel="stylesheet" href="asset/styles/form.css">
     <title>Sign in - Metropolis</title>
 </head>
 <body>
+<?php include("asset/includes/header_visitor.php") ?>
 <div class="wrapper fadeInDown">
   <div id="formContent">
+      
     <!-- Tabs Titles -->
-    <h2 class="active"> Sign In </h2>
+    <h2 class="inactive underlineHover">Sign In </h2>
     <!-- Login Form -->
     <form>
-      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="Login">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="Password">
       <input type="submit" class="fadeIn fourth" value="Log In">
     </form>
 
     <!-- Remind Passowrd -->
     <div id="formFooter">
-      <a class="underlineHover" href="#">Mot de passe oublié ?</a>
+      <a class="underlineHover" href="#">Forgot Password?</a>
     </div>
 
   </div>
--- signup.php
+++ signup.php
@@ -4,24 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="asset/styles/style.css">
+    <link rel="stylesheet" href="asset/styles/form.css">
     <title>Sign up - Metropolis</title>
 </head>
 <body>
-<div class="wrapper fadeInDown">
+    <?php include("asset/includes/header_visitor.php") ?>
+ <div class="wrapper fadeInDown">
   <div id="formContent">
     <!-- Tabs Titles -->
-    <h2 class="active"> Sign Up </h2>
+    <h2 class="inactive underlineHover">Sign Up </h2>
     <!-- Login Form -->
     <form>
-      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
-      <input type="submit" class="fadeIn fourth" value="Log In">
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="Login">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="Password">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="Confirm password">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="E-mail">
+      <input type="submit" class="fadeIn fourth" value="Sign up now !">
     </form>
 
     <!-- Remind Passowrd -->
     <div id="formFooter">
-      <a class="underlineHover" href="#">Mot de passe oublié ?</a>
+      <a class="underlineHover" href="#">Forgot Password?</a>
     </div>
 
   </div>
